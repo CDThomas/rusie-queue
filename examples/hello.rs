fe41a3c09af83c8a177ec98d0ae1dc57028971b3
Drew Thomas <cdthomas.92@gmail.com>
2022-02-28 08:34:52 +1100

    Move worker logic from example to src (#6)

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,22 +1,15 @@
-use futures::{stream, StreamExt};
 use rusie_queue::error::Error;
-use rusie_queue::postgres::*;
+use rusie_queue::postgres::{PostgresJob, PostgresQueue};
+use rusie_queue::worker;
 use serde::{Deserialize, Serialize};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
-use std::fmt::Debug;
 use std::sync::Arc;
 use std::time::Duration;
 
-const CONCURRENCY: usize = 50;
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub enum Message {
-    SendSignInEmail {
-        email: String,
-        name: String,
-        code: String,
-    },
+    SayHello { name: String },
 }
 
 #[tokio::main]
@@ -29,70 +22,24 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let queue = Arc::new(PostgresQueue::new(db.clone()).max_attempts(3));
 
-    // run worker
-    let worker_queue = queue.clone(); // queue is an Arc pointer, so we only copy the reference
-    tokio::spawn(async move { run_worker(worker_queue).await });
+    let worker_queue = queue.clone();
+    tokio::spawn(async move { worker::run(worker_queue, handle_job).await });
 
-    // queue job
-    let job = Message::SendSignInEmail {
-        email: "your@email.com".to_string(),
+    let job = Message::SayHello {
         name: "Rusie Q".to_string(),
-        code: "000-000".to_string(),
     };
-    let _ = queue.push(job, None).await; // TODO: handle error
+
+    queue.push(job, None).await?;
 
     tokio::time::sleep(Duration::from_secs(2)).await;
 
     Ok(())
 }
 
-// TODO: move into rusie_queue
-async fn run_worker(queue: Arc<PostgresQueue>) {
-    loop {
-        let jobs = match queue.pull::<Message>(CONCURRENCY as u32).await {
-            Ok(jobs) => jobs,
-            Err(err) => {
-                println!("run_worker: pulling jobs: {}", err);
-                tokio::time::sleep(Duration::from_millis(500)).await;
-                Vec::new()
-            }
-        };
-
-        let number_of_jobs = jobs.len();
-        if number_of_jobs > 0 {
-            println!("Fetched {} jobs", number_of_jobs);
-        }
-
-        stream::iter(jobs)
-            .for_each_concurrent(CONCURRENCY, |job| async {
-                let job_id = job.id;
-
-                let res = match handle_job(job).await {
-                    Ok(_) => queue.delete_job(job_id).await,
-                    Err(err) => {
-                        println!("run_worker: handling job({}): {}", job_id, &err);
-                        queue.fail_job(job_id).await
-                    }
-                };
-
-                match res {
-                    Ok(_) => {}
-                    Err(err) => {
-                        println!("run_worker: deleting / failing job: {}", &err);
-                    }
-                }
-            })
-            .await;
-
-        // sleep not to overload our database
-        tokio::time::sleep(Duration::from_millis(125)).await;
-    }
-}
-
-async fn handle_job(job: PostgresJob<Message>) -> Result<(), crate::Error> {
+async fn handle_job(job: PostgresJob<Message>) -> Result<(), anyhow::Error> {
     match job.message.0 {
-        message @ Message::SendSignInEmail { .. } => {
-            println!("Sending sign in email: {:?}", &message);
+        Message::SayHello { name } => {
+            println!("Hello, {}", name);
         }
     };
 
